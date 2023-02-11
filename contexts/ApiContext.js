3e8b419f60bd369de8851b9f1c4007e3d4b8b9e4
Vi Pansegrouw <vipansegrouw@gmail.com>
2023-02-11 18:19:02 +0000

    api context

--- contexts/ApiContext.js
+++ contexts/ApiContext.js
@@ -1,13 +1,35 @@
-export const ApiContextState = createContext({});
+import { createContext, useContext, useCallback, useState } from "react";
 
-export const useBuildContext = () => useContext(ApiContextState);
+export const ApiBuildContextState = createContext({});
 
-export const ApiContextProvider = ({ children, config }) => {
-  const [apiBuildContext, setApiBuildContext] = useState({});
-  // TODO
-  const updateApi = useCallback((ApiContentId, traitId) => {
-    const updatedApi = (generate updated stuff)
-    setApiBuildContext(updatedTraits);
-  }
-  return { traits, updateTrait };
+export const useApiBuildContext = () => useContext(ApiBuildContextState);
+
+export const ApiBuildContextProvider = ({ children }) => {
+  const [traitLineData, setTraitLineData] = useState([{}]);
+  const updateTraitLineData = useCallback(( traitLineIndex, id ) => {
+    const apiResponse = useApi({endpoint: 'specializations', id})
+    const newTraitLineData = {
+      specialization_id: id,
+      name: apiResponse.name,
+      minor: apiResponse.minor_traits,
+      major: {
+        adept: apiResponse.major_traits.slice(0,3),
+        master: apiResponse.major_traits.slice(3,6),
+        grandmaster: apiResponse.major_traits.slice(6,9),
+      }
+    }
+    const newApiData = [...traitLineData]
+    newApiData[traitLineIndex] = newTraitLineData
+    setTraitLineData(newApiData);
+  }, [setTraitLineData, traitLineData])
+  return (
+    <ApiBuildContextState.Provider
+      value={{
+        traitLineData,
+        updateTraitLineData,
+      }}
+    >
+      {children}
+    </ApiBuildContextState.Provider>
+  );
 }
